0b251a6cc2d1f6d52397be263db4c17fdce7625b
Rasheed Romain <rasheedromain@Rasheeds-MacBook-Pro.local>
2015-06-03 17:10:54 -0700

    edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,10 +17,32 @@ class PostsController < ApplicationController
 		end 
 	end 
 
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+
+		redirect_to posts_path
+	end
+ 
 	def show 
 		@post = Post.find(params[:id])
 	end 
 
+	def edit 
+		@post = Post.find(params[:id])
+	end 
+
+	def update 
+		@post = Post.find(params[:id])
+		
+		if @post.update(params[:post].permit(:tile, :body))
+			redirect_to root
+		else
+			render 'edit'
+		end 
+	end 
+
+	
 	private 
 		def post_params 
 			params.require(:post).permit(:title, :body)
